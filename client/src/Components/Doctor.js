c195f684822102e2df1fa3e06e89ef7d368ecdee
maestro-7 <52703847+maestro-7@users.noreply.github.com>
2020-05-13 18:54:55 +0530

    Update Doctor.js

--- client/src/Components/Doctor.js
+++ client/src/Components/Doctor.js
@@ -1,44 +1,4 @@
-// import React, { Component } from "react";
 
-// class Doctor extends Component{
-//     contract = this.props.contract;
-//         address = this.props.address;
-//         state={name:null,id:null,patient_list:[]};
-//     // constructor(){
-//     // {
-        
-//     // }
-//     async getinfo()
-//     {
-//         var result;
-//         try{
-//             result = await this.contract['OPT'].methods.getDoctorInfo().call({from :this.Acc[0]});
-//             this.setState({name:result[0],id:result[1],patient_list:result[2]});
-//         }
-//         catch(err)
-//         {
-//             alert('Something went wrong. Login Again');
-//         }
-//     }
-    
-        
-    
-//     render()
-//     {   
-//         this.getinfo();
-//         return(<div className="main">
-//             <div className="Namelbl">Name:</div>
-//             <div className="Name">{this.state.name}</div>
-//             <div className="id">Id:</div>
-//             <div className="id">{this.state.id}</div>
-//             <div className="patlbl">List Of Trated Patients</div>
-//             <div className="patient">{this.state.patient_list.map(patient=><ol>
-//                 <li>patient</li>
-//             </ol>)}</div>
-//         </div>);
-//     }
-// }
-// export default Doctor;
 import React, { Component } from 'react';
 import {  Row, Col, Card, Tag } from 'antd';
 //import { connect } from "react-redux";
@@ -63,11 +23,7 @@ class Doctor extends Component {
             this.loadDoctor();
     }
 
-    // componentWillReceiveProps(nextProps){
-    //     if(healthRecord !== nextProps.global_vars.healthRecord) {
-    //         this.loadDoctor(nextProps.global_vars.healthRecord);   
-    //     }
-    // }
+    
 
     async loadDoctor(){
         let res = await this.healthRecord.methods.getDoctorInfo().call({from :this.Acc[0]});
@@ -120,4 +76,3 @@ const mapStateToProps = (state) => {
 };
 
 export default Doctor;
-//export default connect(mapStateToProps, {})(Doctor);
